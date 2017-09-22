ec61f68d811e4c04b5240291f46893ea951100ee
Mr.Dokepon <Gigabiz.it@gmail.com>
2017-09-22 09:15:02 +0700

    config & theme rkstyle

---
+++ config-appdaemon/custom_css/rkgroup/js/helpers.js
@@ -0,0 +1,18 @@
+$(document).ready(function()
+{
+  $("h1.title2").each(function()
+  {
+    if ($(this).is(":empty"))
+    {
+      $(this).closest("div").children("h1.title").css("line-height","35px");
+    }
+  });
+
+  $(".title").each(function() {
+    if ($(this).is(":empty"))
+    {
+      $(this).css("display","none");
+    }
+  })
+
+});
